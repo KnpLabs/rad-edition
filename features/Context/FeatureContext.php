5877b6998aade5042edf7e42adb8e8f8bd4d549a
Gildas Quéméner <gildas.quemener@knplabs.com>
2012-11-26 13:42:59 +0100

    Creation of the rad edition

---
+++ features/Context/FeatureContext.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Context;
+
+use Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+
+use Behat\Symfony2Extension\Context\KernelDictionary;
+
+class FeatureContext extends BehatContext
+{
+}
